e06509c71506e403eeec8b79244a3dba864515de
Mezősi József Krisztián <mezosijoci@gmail.com>
2025-03-20 19:50:31 +0100

    add revalidate for fetch in getStocksForQuery

--- src/utils/get-stocks-for-query.ts
+++ src/utils/get-stocks-for-query.ts
@@ -1,8 +1,9 @@
 import { QUERY_PARAM } from "@/constants/api-request.constants";
+import { ONE_HOUR_IN_SECONDS } from "@/constants/time.constants";
 import { HTTP_STATUS_CODES, SearchResultItems } from "@/models";
 
 export const getStocksForQuery = async (query: string) => {
-  const res = await fetch(`/api/search?${QUERY_PARAM}=${query}`);
+  const res = await fetch(`/api/search?${QUERY_PARAM}=${query}`, { next: { revalidate: ONE_HOUR_IN_SECONDS } });
   const data = await res.json();
   if (res.status !== HTTP_STATUS_CODES.OK) {
     throw new Error(data);
